bdda077ebac09b758d7370dceec5ab215edd3e4d
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2019-12-08 00:34:28 -0800

    3.4.2 Import CSV

--- PyPoll.py
+++ PyPoll.py
@@ -22,3 +22,29 @@ import datetime
 now = datetime.datetime.now()
 # Print the present time.
 print(f"\nThe time right now is, {now}\n")
+
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+
+#      # To do: perform analysis.
+#      print(election_data)
+
+# # To do: perform analysis.
+
+# # Close the file.
+# election_data.close()
+
+
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
