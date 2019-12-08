c167e6c14e20c52eb533edcb51b957478c53b4a1
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2019-12-07 20:44:32 -0800

    3.2.10 for loops, with items, keys and values

--- phyton_practice.py
+++ phyton_practice.py
@@ -46,12 +46,27 @@
 # else:
 #     print('Your grade is an F.')
 
-counties = ["Arapahoe","Denver","Jefferson"]
-if "El Paso" in counties:
-    print("El Paso is in the list of counties.")
-else:
-    print("El Paso is not the list of counties.")
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not the list of counties.")
 
-for county in counties:
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+print("\nCounties----")
+for county in counties_dict.keys():
     print(county)
-    
+print("\nVoters===")
+for voters in counties_dict.values():
+    print(voters)
+print("\nCounties---- key")
+for county in counties_dict:
+    print(counties_dict[county])
+print("\nCounties---- get")
+for county in counties_dict:
+    # print(county)
+    print(county, counties_dict.get(county))
+
+print("\nCounties---- items")
+for key, value in counties_dict.items():
+    print(key, " has ", value," registered voters.")
