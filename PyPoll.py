8fc1d14a4412f5b669a00c0a00113b9eb7365361
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2019-12-07 23:28:40 -0800

    3.4.1 CSV election algorithm and datetime module, class, method

---
+++ PyPoll.py
@@ -0,0 +1,24 @@
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+# Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
+# 1. Open the data file.
+# 2. Write down the names of all the candidates.
+# 3. Add a vote count for each candidate.
+# 4. Get the total votes for each candidate.
+# 5. Get the total votes cast for the election.
+
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candicate won
+# 5. the winner of the election based on popular vote
+
+# Import the datetime dependency.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print(f"\nThe time right now is, {now}\n")
--- phyton_practice.py
+++ phyton_practice.py
@@ -52,21 +52,76 @@
 # else:
 #     print("El Paso is not the list of counties.")
 
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-print("\nCounties----")
-for county in counties_dict.keys():
-    print(county)
-print("\nVoters===")
-for voters in counties_dict.values():
-    print(voters)
-print("\nCounties---- key")
-for county in counties_dict:
-    print(counties_dict[county])
-print("\nCounties---- get")
-for county in counties_dict:
-    # print(county)
-    print(county, counties_dict.get(county))
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# print("\nCounties----")
+# for county in counties_dict.keys():
+#     print(county)
+# print("\nVoters===")
+# for voters in counties_dict.values():
+#     print(voters)
+# print("\nCounties---- key")
+# for county in counties_dict:
+#     print(counties_dict[county])
+# print("\nCounties---- get")
+# for county in counties_dict:
+#     # print(county)
+#     print(county, counties_dict.get(county))
 
-print("\nCounties---- items")
-for key, value in counties_dict.items():
-    print(key, " has ", value," registered voters.")
+# print("\nCounties---- items")
+# for key, value in counties_dict.items():
+#     print(key, " has ", value," registered voters.")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+# for county_dict in voting_data:
+#     print(county_dict)
+# print("\nVoting data === List using range")
+# for i in range(len(voting_data)):
+#     print(voting_data[i])
+
+# print("\nNested loop=== using Values")
+# for county_dict in voting_data:
+#     for value in county_dict.values():
+#         print(value)
+
+
+# print("\nNested loop=== using items")
+# for county_dict in voting_data:
+#     for key, value in county_dict.items():
+#         print(value)
+
+print("\n loop=== registered voters = list")
+for county_dict in voting_data:
+#    print(county_dict['registered_voters'])
+    print(f"{county_dict['county']} county has {county_dict['registered_voters']} registered voters.")
+        
+# print("\n loop=== counties")
+# for county_dict in voting_data:
+#     print(county_dict['county'])
+
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+
+# print("\nRegular Print")
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# print("\nPrint Formatted")
+# print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+print("\n Counties === sample")
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    # print(county + " county has " + str(voters) + " registered voters.")
+    print(f"{county} county has {voters} registered voters.")
+
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# # :{10}.{2}
+# message_to_candidate = (
+#     f"You received {candidate_votes} number of votes. "
+#     f"The total number of votes in the election was {total_votes}. "
+#     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+# print("\n", message_to_candidate,"\n")
