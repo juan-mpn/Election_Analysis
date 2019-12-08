eca5ad6e616d06c66f9b0b7e6a0a2966def77af9
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2019-12-08 09:24:06 -0800

    3.4.3 Write to files

--- PyPoll.py
+++ PyPoll.py
@@ -48,3 +48,24 @@ with open(file_to_load) as election_data:
 
     # Print the file object.
      print(election_data)
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the with statement open the file as a text file.
+# outfile = open(file_to_save, "w")
+# # Write some data to the file.
+# outfile.write("Hello World")
+
+# # Close the file
+# outfile.close()
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+print("Open using ---- with")
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
